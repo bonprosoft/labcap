35f7e69e8870fe2cac87fe1d2e31492ddf283972
Yuki IGARASHI <me@bonprosoft.com>
2016-04-13 16:50:32 +0900

    Fix bug.
    エンコーディングの処理をミスっていたので修正、日本語名に対応

--- server/slack_notify.py
+++ server/slack_notify.py
@@ -12,14 +12,14 @@ def create_timespan_string(timespan):
     return "%d日%d時間%d分" % (timespan.days, timespan.seconds / (60 * 60), timespan.seconds % (60 * 60) / 60)
 
 def notify_active(username, timespan):
-    item = {"text": "%sさんが入室しました。(前回の退室から %s ぶり)" % (str(username), create_timespan_string(timespan)), "username": USERNAME, "icon_emoji": ":ghost:", "channel": CHANNEL }
+    item = {"text": "%sさんが入室しました。(前回の退室から %s ぶり)" % (username.encode("utf-8"), create_timespan_string(timespan)), "username": USERNAME, "icon_emoji": ":ghost:", "channel": CHANNEL }
     try:
         r = requests.post(url, data = json.dumps(item))
     except Exception as e:
         pass
 
 def notify_deactive(username, timespan):
-    item = {"text": "%sさんが退室しました。(今回の滞在時間: %s)" % (str(username), create_timespan_string(timespan)), "username": USERNAME, "icon_emoji": ":ghost:", "channel": CHANNEL }
+    item = {"text": "%sさんが退室しました。(今回の滞在時間: %s)" % (username.encode("utf-8"), create_timespan_string(timespan)), "username": USERNAME, "icon_emoji": ":ghost:", "channel": CHANNEL }
     try:
         r = requests.post(url, data = json.dumps(item))
     except Exception as e:
